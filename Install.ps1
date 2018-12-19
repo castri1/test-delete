81f34948d75b86c5dd68c4d9a6322f5e8c79589b
Castri1 <daniel.castrillon@vaadoit.com>
2018-12-19 13:47:56 -0500

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -4,4 +4,8 @@ Set-Location 'C:\Deployment'
 & dotnet.exe restore
 
 #Publish application
-& dotnet.exe publish -c release -o $publish_path -f netcoreapp2.1 -r win-x64
+& dotnet.exe publish -c release -o 'C:\Deployment\publish' -f netcoreapp2.1 -r win-x64
+
+# Point IIS wwwroot of the published folder. CodeDeploy uses 32 bit version of PowerShell.
+# To make use the IIS PowerShell CmdLets we need call the 64 bit version of PowerShell.
+C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value c:\Deployment\publish}
